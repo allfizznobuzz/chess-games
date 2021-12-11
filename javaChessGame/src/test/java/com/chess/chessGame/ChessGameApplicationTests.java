3d0624c2528cd1fa623a475d013bd4e9066e12cb
John Hurdle <johnhurdle@johns-mbp.home>
2021-12-10 21:59:30 -0500

    Pushing starter code

---
+++ javaChessGame/src/test/java/com/chess/chessGame/ChessGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.chessGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
